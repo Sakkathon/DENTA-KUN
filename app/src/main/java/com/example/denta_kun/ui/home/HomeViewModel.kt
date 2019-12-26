5206e4a98bfb99147a90cc39731d208184c3f2df
rikusen0335 <rikusen0335@gmail.com>
2019-12-26 11:41:32 +0900

    initial commit including gitignore

---
+++ app/src/main/java/com/example/denta_kun/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.denta_kun.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
